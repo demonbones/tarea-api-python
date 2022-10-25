5431c345789d831f7cd55d0b5eb10644a4af1dfd
demonbones <stivendrada@hotmail.com>
2022-10-24 21:03:40 -0500

    laboratorio de solicitud api con python

---
+++ mapquest_parse-json_2.py
@@ -0,0 +1,18 @@
+import urllib.parse
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+orig = "Rome, Italy"
+dest = "Frascati, Italy"
+key = "YAgNSPMil6Z4ARGclfFAEElXGMEKok4s"
+
+url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
+
+print("URL: " + (url))
+
+json_data = requests.get(url).json()
+json_status = json_data["info"]["statuscode"]
+
+
+if json_status == 0:
+    print("API Status: " + str(json_status) + " = A successful route call.\n")
